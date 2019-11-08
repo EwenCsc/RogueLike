0b0188910761eaa029583da51c6c7d42d6e3017f
Ewen Cossec <cossec.ewen@gmail.com>
2019-11-08 23:18:04 +0100

    Amelioration de la génration. Mais salles trop regrouper (ajouter une proba de remplissage selon le nombres de voisins).

--- MapRogueLike/Map.cs
+++ MapRogueLike/Map.cs
@@ -14,23 +14,58 @@ namespace MapRogueLike
         int nbRooms = 15;
         Room[,] rooms;
 
+        public Vector2 MapSize => mapSize;
+        public Room[,] Rooms => rooms;
+
         public Map()
         {
             GraphicsDeviceManager gd = Tool.GraphicsDeviceManager;
             Vector2 center = new Vector2((gd.PreferredBackBufferWidth / 2) / (int)Room.Size.X, (gd.PreferredBackBufferHeight / 2) / (int)Room.Size.Y);
 
+            // Init Rooms
             rooms = new Room[(int)mapSize.X, (int)mapSize.Y];
-
-            Random rnd;
-            for(int i = 0; i < nbRooms; i++)
+            for (int i = 0; i < rooms.GetLength(0); i++)
             {
+                for (int j = 0; j < rooms.GetLength(0); j++)
+                {
+                    rooms[i, j] = new Room(new Vector2(i, j), true);
+                }
             }
-            //rooms.Add(new Room(new Vector2(gd.PreferredBackBufferWidth / 2, gd.PreferredBackBufferHeight / 2)));
+
+            // Generating Map
+            Random rnd;
+            for (int i = 0; i < rooms.GetLength(0); i++)
+            {
+                List<Room> setedRooms = rooms.Cast<Room>().ToList().FindAll(x => !x.isEmpty);
+                if (setedRooms.Count == 0)
+                {
+                    rooms[(int)center.X, (int)center.Y] = new Room(center);
+                    Console.WriteLine("{0} -> ({1}, {2})", i, center.X, center.Y);
+                }
+                else
+                {
+                    rnd = new Random();
+                    List<Room> notFullNeighbouredRooms = setedRooms.FindAll(x => !x.IsFullNeighboured(this));
+                    Room selectedRoom = notFullNeighbouredRooms[rnd.Next(0, notFullNeighbouredRooms.Count)];
+
+                    rnd = new Random();
+                    List<Room> freeNeighbours = selectedRoom.GetFreeNeighbours(this);
+                    Vector2 newEmplacement = freeNeighbours[rnd.Next(0, freeNeighbours.Count)].Emplacement;
+                    rooms[(int)newEmplacement.X, (int)newEmplacement.Y] = new Room(newEmplacement);
+                    Console.WriteLine("{0} -> ({1}, {2})", i, (int)newEmplacement.X, (int)newEmplacement.Y);
+                }
+            }
         }
 
         public void Draw(SpriteBatch spriteBatch)
         {
-            rooms.ToList().ForEach(x => x.Draw(spriteBatch));
+            for (int i = 0; i < rooms.GetLength(0); i++)
+            {
+                for (int j = 0; j < rooms.GetLength(0); j++)
+                {
+                    rooms[i, j].Draw(spriteBatch);
+                }
+            }
         }
     }
 }
--- MapRogueLike/Room.cs
+++ MapRogueLike/Room.cs
@@ -7,33 +7,74 @@ namespace MapRogueLike
 {
     public class Room
     {
+        public bool isEmpty = false;
         Vector2 emplacement;
         static readonly Vector2 tilingDimension = new Vector2(16, 9);
         public static Vector2 Size => new Vector2(tilingDimension.X * Tile.size.X, tilingDimension.Y * Tile.size.Y);
 
         List<Tile> tiles = new List<Tile>();
-
+        
         public Vector2 Emplacement => emplacement;
         //public Vector2 Position => new Vector2(emplacement.X + (emplacement.X * (Size.X  - 1)), emplacement.Y + (emplacement.Y * (Size.Y - 1)));
         public Vector2 Position => new Vector2(emplacement.X + (emplacement.X * Size.X), emplacement.Y + (emplacement.Y * Size.Y)); // Séparation entre les salles de 1px (jolie)
 
-        public Room(Vector2 _empalcement)
+        public Room(Vector2 _empalcement, bool _isEmpty = false)
         {
+            isEmpty = _isEmpty;
             emplacement = _empalcement;
-            for (int i = 0; i < tilingDimension.X; i++)
+            if (!isEmpty)
             {
-                for (int j = 0; j < tilingDimension.Y; j++)
+                for (int i = 0; i < tilingDimension.X; i++)
                 {
-                    tiles.Add(new Tile(new Vector2(i, j), this));
+                    for (int j = 0; j < tilingDimension.Y; j++)
+                    {
+                        tiles.Add(new Tile(new Vector2(i, j), this));
+                    }
                 }
             }
         }
 
+        internal bool IsFullNeighboured(Map map)
+        {
+            return (
+                (emplacement.X + 1 < map.MapSize.X && !map.Rooms[(int)emplacement.X + 1, (int)emplacement.Y].isEmpty) && 
+                (emplacement.X - 1 >= 0            && !map.Rooms[(int)emplacement.X - 1, (int)emplacement.Y].isEmpty) && 
+                (emplacement.Y + 1 < map.MapSize.Y && !map.Rooms[(int)emplacement.X, (int)emplacement.Y + 1].isEmpty) && 
+                (emplacement.Y - 1 >= 0            && !map.Rooms[(int)emplacement.X, (int)emplacement.Y - 1].isEmpty));
+        }
+
+        internal List<Room> GetFreeNeighbours(Map map)
+        {
+            List<Room> freeNeighbours = new List<Room>();
+            Console.WriteLine("Selected Room -> ({0}, {1})", emplacement.X, emplacement.Y);
+            if (emplacement.X + 1 < map.MapSize.X && map.Rooms[(int)emplacement.X + 1, (int)emplacement.Y].isEmpty)
+            {
+                freeNeighbours.Add(map.Rooms[(int)emplacement.X + 1, (int)emplacement.Y]);
+            }
+            if (emplacement.X - 1 >= 0 && map.Rooms[(int)emplacement.X - 1, (int)emplacement.Y].isEmpty)
+            {
+                freeNeighbours.Add(map.Rooms[(int)emplacement.X - 1, (int)emplacement.Y]);
+            }
+            if (emplacement.Y + 1 < map.MapSize.Y && map.Rooms[(int)emplacement.X, (int)emplacement.Y + 1].isEmpty)
+            {
+                freeNeighbours.Add(map.Rooms[(int)emplacement.X, (int)emplacement.Y + 1]);
+            }
+            if (emplacement.Y - 1 >= 0 && map.Rooms[(int)emplacement.X, (int)emplacement.Y - 1].isEmpty)
+            {
+                freeNeighbours.Add(map.Rooms[(int)emplacement.X, (int)emplacement.Y - 1]);
+            }
+            freeNeighbours.ForEach(x => Console.WriteLine("({0}, {1})", x.Emplacement.X, x.Emplacement.Y));
+            return freeNeighbours;
+        }
+
         public void Draw(SpriteBatch spriteBatch)
         {
-            foreach (Tile tile in tiles)
+            if (!isEmpty)
             {
-                tile.Draw(spriteBatch);
+                foreach (Tile tile in tiles)
+                {
+                    tile.Draw(spriteBatch);
+                }
             }
         }
     }
